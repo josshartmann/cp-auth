9da6a4a316da8960a9014dae832056ff0fb806d9
Josias Hartmann <josshartmann@gmail.com>
2023-05-19 10:28:04 -0300

    Exclusão de código comentado

--- cp_auth/urls.py
+++ cp_auth/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 from .api import facebook, google
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("profile/", include('cp_profile.urls')),
